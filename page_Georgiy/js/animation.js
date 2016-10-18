6bd142ea21bd3024b2544c7a5cc02bf7584677a2
Georg <formrnice@yandex.ru>
2016-10-18 20:07:14 +0300

    added all files for my page(Georgiy)

---
+++ page_Georgiy/js/animation.js
@@ -0,0 +1,26 @@
+// Запускаем анимацию
+ onload=function (){  
+
+   $("#senoText").fadeIn(1000);
+   new WOW().init();
+  //$("#seno1").animate({left: '+=1300px'}, 2000);
+   
+
+   
+// Останавливаем анимацию
+   
+/* $("#stop").click(function(){
+   
+  $(".block").stop();
+   
+});
+   
+// Запускаем в другом направлении
+   
+$("#back").click(function(){
+   
+  $(".block").animate({left: '-=100px'}, 2000);
+   
+}); */
+
+}
